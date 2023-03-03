a5782f0d644817bba6be5c89826086a9f91a9ef1
pedropaulofb <pedropaulofb@gmail.com>
2023-03-03 02:43:22 -0300

    list of valid taxonomies created

--- src/modules/validation/validation_functions.py
+++ src/modules/validation/validation_functions.py
@@ -12,10 +12,10 @@ from src.modules.validation.validation_queries import QUERIES_LIST
 class validated_taxonomy:
     """ Class to store the results for each evaluated taxonomy. """
 
-    def __init__(self, file_name, list_problems, problems_bool):
+    def __init__(self, file_name, list_problems_queries, list_problems_count):
         self.file_name = file_name
-        self.list_problems = list_problems
-        self.problems_bool = problems_bool
+        self.list_problems_queries = list_problems_queries
+        self.list_problems_count = list_problems_count
 
 
 def save_csv_validation(queries_list, evaluated_taxonomies):
@@ -32,9 +32,39 @@ def save_csv_validation(queries_list, evaluated_taxonomies):
     csv_header.insert(0, "file_name")
 
     for taxonomy_item in evaluated_taxonomies:
-        row = taxonomy_item.problems_bool
+        row = taxonomy_item.list_problems_count
         row.insert(0, taxonomy_item.file_name)
         write_csv_row(file_name, csv_header, row)
+
+
+def create_valid_lists(evaluated_taxonomies):
+    """ Generates two files:
+    valid_taxonomies_c.txt: list of all valid taxonomies considering closed-world assumption (complete models)
+    valid_taxonomies_n.txt: list of all valid taxonomies considering open-world assumption (incomplete models)
+    """
+
+    logger = initialize_logger()
+
+    valid_taxonomies_c_file = "valid_taxonomies_c.txt"
+    valid_taxonomies_n_file = "valid_taxonomies_n.txt"
+
+    list_valid_c = []
+    list_valid_n = []
+
+    for taxonomy in evaluated_taxonomies:
+        if not taxonomy.list_problems_queries:
+            list_valid_c.append(taxonomy.file_name)
+            list_valid_n.append(taxonomy.file_name)
+        elif taxonomy.list_problems_queries == ["L13"]:
+            list_valid_n.append(taxonomy.file_name)
+
+    logger.info(f"Writing {valid_taxonomies_c_file}")
+    with open(valid_taxonomies_c_file, 'w') as tfile:
+        tfile.write('\n'.join(list_valid_c))
+    logger.info(f"Writing {valid_taxonomies_n_file}")
+    with open(valid_taxonomies_n_file, 'w') as tfile:
+        tfile.write('\n'.join(list_valid_n))
+    logger.info(f"List of valid taxonomies successfully written")
 
 
 def validate_gufo_taxonomies():
@@ -62,14 +92,18 @@ def validate_gufo_taxonomies():
         # Performing queries
         for validation_query in QUERIES_LIST:
             query_result = taxonomy_graph.query(QUERIES_LIST[validation_query])
-            problems_found = len(query_result)
-            current_taxonomy.problems_bool.append(problems_found)
+            number_problems_found = len(query_result)
+            current_taxonomy.list_problems_count.append(number_problems_found)
 
-            if problems_found > 0:
-                current_taxonomy.list_problems.append(validation_query)
+            if number_problems_found > 0:
+                current_taxonomy.list_problems_queries.append(validation_query)
 
-        if len(current_taxonomy.list_problems) > 0:
-            logger.warning(f"Identified violation(s) of constraint(s): {current_taxonomy.list_problems}")
+        if len(current_taxonomy.list_problems_queries) > 0:
+            logger.warning(f"Identified violation(s) of constraint(s): {current_taxonomy.list_problems_queries}")
 
     logger.info(f"Validation of {len(list_all_files)} taxonomies successfully concluded\n")
+
+    # Creating file validation.csv
     save_csv_validation(QUERIES_LIST, evaluated_taxonomies)
+    # Creating files valid_taxonomies_c.csv and valid_taxonomies_n.csv
+    create_valid_lists(evaluated_taxonomies)
